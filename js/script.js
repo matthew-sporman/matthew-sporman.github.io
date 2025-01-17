bcaa5b0ff4b29548607b19a391807525c76e8dae
Matthew Sporman <mfsporma@mtu.edu>
2025-01-16 21:10:20 -0500

    "Contact Me" Form
    Completion of coding HTML, CSS & JS for the Contact Me section of webpage.

--- js/script.js
+++ js/script.js
@@ -5,20 +5,36 @@ document.addEventListener("DOMContentLoaded", function() {
     // Get all the navigation links
     const navLinks = document.querySelectorAll('nav a');
 
+    // Get all sections
+    const sections = document.querySelectorAll("section");
+
+
+    // Function to show a specific section
+    function showSection(targetId) {
+        // Hide all sections
+        sections.forEach(section => section.style.display = "none");
+
+        const targetSection = document.getElementById(targetId);
+        
+        // Show the targeted section
+        if (targetSection) {
+            targetSection.style.display = "block";
+        }
+    }
+    
     // Add an event listener for each link
     navLinks.forEach(link => {
         link.addEventListener('click', function(e) {
             e.preventDefault();  // Prevent the default anchor link behavior
 
             // Hide all sections
-            const sections = document.querySelectorAll('main section');
-            sections.forEach(section => {
-                section.style.display = 'none';
-            });
+            sections.forEach(section => section.style.display = "none");
 
-            // Show the section corresponding to the clicked link
-            const targetSection = document.querySelector(this.getAttribute('href'));
-            targetSection.style.display = 'block';
+            const targetId = link.getAttribute("href").substring(1);
+
+            showSection(targetId);
         });
     });
+
+    showSection("home");
 });
